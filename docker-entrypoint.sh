2a56743c8ae1efa508da5d48f670de48ad1d1833
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-04-02 21:54:15 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
-NODE_NAME=$(cat /etc/nodename)
-echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/rpi_exporter/node-meta.prom
+if [ ! -f "/etc/nodename" ] then
+  NODE_NAME=$(cat /etc/nodename)
+  echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/rpi_exporter/node-meta.prom
 
-set -- /bin/rpi_exporter "$@"
+  set -- /bin/rpi_exporter "$@"
 
-exec "$@"
+  exec "$@"
+fi
