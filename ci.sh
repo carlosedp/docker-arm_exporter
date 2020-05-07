0814fe2ff47a461495917f790c284f1fac2aaec0
Carlos Eduardo <me@carlosedp.com>
2020-05-07 14:52:50 -0300

    Test PR (#8)

--- ci.sh
+++ ci.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
     docker buildx build \
     --progress plain \
     --platform=$ARCHS \
     .
     exit $?
 fi
-TAG="${TRAVIS_TAG:-latest}"
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker buildx build \
-    --progress plain \
-    --platform=$ARCHS \
-    -t $DOCKER_REPO:$TAG \
-    --push \
-    .
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    TAG="${TRAVIS_TAG:-latest}"
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    docker buildx build \
+        --progress plain \
+        --platform=$ARCHS \
+        -t $DOCKER_REPO:$TAG \
+        --push \
+        .
+fi
